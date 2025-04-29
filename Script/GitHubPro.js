0ce9f350fac95b03fdd179516a28dae785ab4138
guoguanhua <guogh2000@outlook.com>
2025-04-29 15:32:10 +0800

    update

--- Script/GitHubPro.js
+++ Script/GitHubPro.js
@@ -6,8 +6,21 @@ let config = {
 const username = $request.url.match(
   /https:\/\/(?:raw|gist)\.githubusercontent\.com\/([^\/]+)\//
 )[1];
-// rewrite headers for specific user
+
 if (username == config.username) {
   console.log(`ACCESSING PRIVATE REPO: ${$request.url}`);
-  $done({ headers: {...$request.headers, Authorization: `token ${config.token}`} });
-} else $done({});
+  $done({
+    headers: {
+      ...$request.headers,
+      Authorization: `token ${config.token}`,
+      "Accept-Language": `en-us`
+    }
+  });
+} else {
+  $done({
+    headers: {
+      ...$request.headers,
+      "Accept-Language": `en-us`
+    }
+  });
+}
